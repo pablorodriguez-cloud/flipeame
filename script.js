b649fc4e00d73934607aa728d0fc3d19cd6f6c7d
pablorodriguez-cloud <rodriguezguardapablo@gmail.com>
2025-11-25 14:47:26 -0300

    Increase description length limit to 800 characters

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ function renderWebPreview(data) {
 
           <div class="property-section">
             <h3>Descripción Ejecutiva</h3>
-            <p>${descripcion.substring(0, 400)}${descripcion.length > 400 ? '...' : ''}</p>
+            <p>${descripcion.substring(0, 800)}${descripcion.length > 800 ? '...' : ''}</p>
           </div>
         </div>
       </div>
